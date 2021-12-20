bd6703ebe25ef2436f8de1aca19014017def5984
Ethan Saxenian <esaxenian@middlebury.edu>
2021-12-20 17:53:43 -0500

    fix accordion

--- src/components/CardSearcher.js
+++ src/components/CardSearcher.js
@@ -10,14 +10,14 @@ export default function CardSearcher({ cards }) {
   const filteredCards = cards.filter(({ name, expansion, types, cost, text }) => {
     const parsedTerm = searchTerm.toLowerCase();
     return ((name.toLowerCase().includes(parsedTerm)
-    || expansion.toLowerCase().includes(parsedTerm)
-    || types.some((type) => type.toLowerCase().includes(parsedTerm))
-    || (cost && (
-      (cost.coins && cost.coins.toString().includes(parsedTerm))
-      || (cost.potions && cost.potions.includes(parsedTerm))
-      || (cost.debt && cost.debt.includes(parsedTerm))
-    ))
-    || text.toLowerCase().includes(parsedTerm)))
+			|| expansion.toLowerCase().includes(parsedTerm)
+			|| types.some((type) => type.toLowerCase().includes(parsedTerm))
+			|| (cost && (
+				(cost.coins && cost.coins.toString().includes(parsedTerm))
+				|| (cost.potions && cost.potions.includes(parsedTerm))
+				|| (cost.debt && cost.debt.includes(parsedTerm))
+			))
+			|| text.toLowerCase().includes(parsedTerm)))
   });
 
   const sortedCards = filteredCards.sort((card1, card2) => sortTwoCards(card1, card2, sortBy))

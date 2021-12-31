3f7c10093e9706a72b9e0a42f8e77980e108d943
Ethan Saxenian <esaxenian@middlebury.edu>
2021-12-31 12:42:49 -0500

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from 'theme/theme.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render((
+	<ChakraProvider theme={theme}>
+		<App/>
+	</ChakraProvider>
+), document.getElementById('root'));

02905d89bc9fd6929124d6a0ceb760226744e3e1
Ethan Saxenian <esaxenian@middlebury.edu>
2022-04-04 21:06:14 -0400

    react 18 changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from 'components/App';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from 'theme/theme.js';
 import cards from 'data/dominion_cards.json';
 import { CardProvider } from 'context.js';
 
-ReactDOM.render((
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
 	<ChakraProvider theme={theme}>
 		<CardProvider value={{cards}}>
 			<App/>
 		</CardProvider>
 	</ChakraProvider>
-), document.getElementById('root'));
+);

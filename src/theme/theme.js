365c8068398da22b61460d1cc6180727e7b02e3b
Ethan Saxenian <esaxenian@middlebury.edu>
2022-04-19 17:27:19 -0400

    spacing

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,23 +1,23 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-	styles: {
-		global: {
-			'*, *::before, *::after': {
-				boxSizing: 'border-box'
-			},
-			'*': {
-				margin: 0
-			},
-			'html, body': {
-				height: '100%'
-			},
-			'img, picture, video, canvas, svg': {
-				display: 'block',
-				maxWidth: '100%'
-			}
-		}
-	}
+  styles: {
+    global: {
+      '*, *::before, *::after': {
+        boxSizing: 'border-box'
+      },
+      '*': {
+        margin: 0
+      },
+      'html, body': {
+        height: '100%'
+      },
+      'img, picture, video, canvas, svg': {
+        display: 'block',
+        maxWidth: '100%'
+      }
+    }
+  }
 });
 
 export default theme;

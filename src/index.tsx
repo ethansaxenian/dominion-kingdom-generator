e7f69d8621a7cc6580742e266b8b11a443d6d368
Ethan Saxenian <ethansaxenian@gmail.com>
2022-10-15 12:48:56 -0400

    update deps

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { Provider } from 'react-redux';
 import { theme } from 'theme';
 import { store } from 'state';
-
 import { createRoot } from 'react-dom/client';
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(

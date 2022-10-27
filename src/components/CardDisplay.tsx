ac9af377bca789fe3080cd50cf846e81405503c3
Ethan Saxenian <ethansaxenian@gmail.com>
2022-10-27 10:55:53 -0400

    adjust for better api and include better fallback card image

--- src/components/CardDisplay.tsx
+++ src/components/CardDisplay.tsx
@@ -2,15 +2,17 @@ import {
   AspectRatio,
   Box,
   HStack,
+  Highlight,
   Image,
   LinkOverlay,
   Spinner,
   Tag,
+  Text,
 } from '@chakra-ui/react';
 import { Card, isLandscape } from 'lib';
 import { SwapCardButton } from './SwapCardButton';
 import { LockCardButton } from './LockCardButton';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 export interface CardDisplayProps {
   card: Card;
@@ -25,7 +27,12 @@ export const CardDisplay: FC<CardDisplayProps> = ({
   lock,
   blackMarket,
 }) => {
+  const [showNameFallback, setShowNameFallback] = useState(true);
+
   const cardWidth = isLandscape(card) ? '72' : '44';
+  const fallbackImg = isLandscape(card)
+    ? 'card-back-landscape.png'
+    : 'card-back.png';
 
   return (
     <Box w={cardWidth} borderRadius="8px" position="relative">
@@ -36,12 +43,29 @@ export const CardDisplay: FC<CardDisplayProps> = ({
         <LinkOverlay isExternal href={card.link}>
           <Image
             w={cardWidth}
-            src={`data:image/jpeg;base64,${card.img_b64}`}
+            src={card.img_path}
             alt={card.name}
             border="5px solid black"
             borderRadius="8px"
-            fallback={<Spinner size="xl" />}
+            fallbackSrc={`${process.env.PUBLIC_URL}/${fallbackImg}`}
+            onLoad={() => setShowNameFallback(false)}
           />
+          {showNameFallback ? (
+            <Highlight
+              query={card.name}
+              styles={{
+                position: 'absolute',
+                px: '1',
+                bg: 'white',
+                borderRadius: '8px',
+                fontSize: 16,
+                fontWeight: 700,
+                opacity: 0.8,
+              }}
+            >
+              {card.name}
+            </Highlight>
+          ) : undefined}
         </LinkOverlay>
       </AspectRatio>
       {swap && (
